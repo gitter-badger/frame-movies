1644f743996c950264a4be58ba0c58b013102828
Simon Walker <s.r.walker101@googlemail.com>
2015-03-20 16:50:42 +0000

    Add test for generating movie

---
+++ testing/test_generate_movie.py
@@ -0,0 +1,14 @@
+import sys
+import os
+sys.path.insert(0, os.path.realpath('.'))
+import mimetypes
+
+from create_movie import generate_movie
+
+
+def test_generate_movie(tmpdir):
+    images_dir = 'images'
+    output_filename = tmpdir.join('out.mp4')
+    generate_movie(images_dir, str(output_filename))
+    assert (os.path.isfile(str(output_filename)) and
+            mimetypes.guess_type(str(output_filename)) == ('video/mp4', None))
